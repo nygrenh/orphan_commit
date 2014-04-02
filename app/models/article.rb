fba07b218a08a33dfd96632cd5ef9d478cab7924
Eemi <eemi.haukkala@gmail.com>
2014-04-02 22:46:00 +0300

    added validations and unit test to Articles-model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,26 @@
 class Article < ActiveRecord::Base
-  validates_presence_of :author
-  validates_presence_of :title
-  validates_presence_of :journal
-  validates_presence_of :year
-  validates_presence_of :volume
-  validates_presence_of :number
-  validates_presence_of :abbreviation
-  validates_presence_of :pages
+  validates :author,        presence: true
+
+  validates :title,         presence: true
+
+  validates :journal,       presence: true
+
+  validates :year,          presence: true,
+                            numericality: {less_than_or_equal_to: ->(_) { Time.now.year },
+                                           greater_than_or_equal_to: 0}
+
+  validates :volume,        presence: true,
+                            numericality: {only_integer: true,
+                                           greater_than_or_equal_to: 1}
+
+  validates :number,        presence: true,
+                            numericality: {only_integer: true,
+                                           greater_than_or_equal_to: 1}
+
+  validates :abbreviation,  presence: true
+
+  validates :pages,         presence: true
+
 
   def to_s
     author + ". " + title + ". " + journal + ", " + volume.to_s + "(" + number.to_s + "):" + pages + ", " + year.to_s
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,1 +1,72 @@
 require 'spec_helper'
+
+describe "Articles" do
+
+  it "count is initially zero" do
+    expect(Article.count).to eq(0)
+  end
+
+  describe "after adding one article" do
+
+
+    it "correctly, count increases by one" do
+      FactoryGirl.create(:article)
+      expect(Article.count).to eq(1)
+    end
+
+    describe "(that is invalid)" do
+
+      it "with a negative Volume, count doesnt increase" do
+        artcl = FactoryGirl.build(:article)
+        artcl.volume = -1
+        artcl.save
+
+        expect(Article.count).to eq(0)
+      end
+
+      it "with a Volume value of 0, count doesnt increase" do
+        artcl = FactoryGirl.build(:article)
+        artcl.volume = 0
+        artcl.save
+
+        expect(Article.count).to eq(0)
+      end
+
+      it "with a negative value for Number, count doesnt increase" do
+        artcl = FactoryGirl.build(:article)
+        artcl.number = -1
+        artcl.save
+
+        expect(Article.count).to eq(0)
+      end
+
+      it "with a Number value of 0, count doesnt increase" do
+        artcl = FactoryGirl.build(:article)
+        artcl.number = 0
+        artcl.save
+
+        expect(Article.count).to eq(0)
+      end
+
+      it "with negative Year, count doesnt increase" do
+        artcl = FactoryGirl.build(:article)
+        artcl.year = -1
+        artcl.save
+
+        expect(Article.count).to eq(0)
+      end
+
+      it "FROM THE FUTURE, count doesnt increase" do
+        artcl = FactoryGirl.build(:article)
+        artcl.year = Time.now.year + 1000
+        artcl.save
+
+        expect(Article.count).to eq(0)
+      end
+
+    end
+
+  end
+
+end
+
