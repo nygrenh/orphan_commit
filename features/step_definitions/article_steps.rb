63cd6ac123d03aeb793fee337319fc02d724b5eb
Eemi <eemi.haukkala@gmail.com>
2014-04-03 04:24:40 +0300

    refactorattu pois virheelliset Article-attribuutit

--- features/step_definitions/article_steps.rb
+++ features/step_definitions/article_steps.rb
@@ -9,16 +9,16 @@ Given(/^I am on new article reference page$/) do
 end
 
 Given(/^I fill in all the fields with correct input values$/) do
-  fill_in('article_abbreviation', with: 'W04')
   fill_in('article_author', with: 'Whittington, Keith J.')
   fill_in('article_title', with: 'Infusing active learning into introductory programming courses')
   fill_in('article_journal', with: 'J. Comput. Small Coll.')
+  fill_in('article_year', with: '2004')
   fill_in('article_volume', with: '19')
   fill_in('article_number', with: '5')
-  fill_in('article_year', with: '2004')
   fill_in('article_pages', with: '249--259')
-  fill_in('article_publisher', with: 'Consortium for Computing Sciences in Colleges')
-  fill_in('article_address', with: 'USA')
+  fill_in('article_month', with: '3')
+  fill_in('article_note', with: 'IMPORTANT')
+  fill_in('article_key', with: 'W04')
 end
 
 When(/^I press "(.*?)"$/) do |arg1|
