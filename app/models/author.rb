3aaba68dd781654cfcb58f9cb12539e79421b993
Eemi <eemi.haukkala@gmail.com>
2014-04-07 20:55:43 +0300

    kaikki toimii manuaalitestatessa mutta cucumberit valittaa

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,7 @@
 class Author < ActiveRecord::Base
   has_many :reference_authors
+  has_many :reference_editors
   has_many :references, through: :reference_authors
+  has_many :references, through: :reference_editors
 
 end
