463950b58e6e883c6dd0bed4f6070c5183dfbe66
Sonopa <juzax@hotmail.com>
2014-04-08 16:24:32 +0300

    Bibtex special chars added

--- app/models/concerns/bibtex.rb
+++ app/models/concerns/bibtex.rb
@@ -6,34 +6,38 @@ module Bibtex
     if key == ""
       bibtex += "\n"
     else
-      bibtex += key + ",\n"
+      bibtex += special_chars(key) + ",\n"
     end
     bibtex += "author = {"
     authors.each_with_index do |author, index|
       if (index+1 == authors.length)
-        bibtex += author.name + "},\n"
+        bibtex += special_chars(author.name) + "},\n"
       else
-        bibtex += author.name + "and"
+        bibtex += special_chars(author.name) + "and"
       end
     end
     attributes.each_pair do |name, value|
     unless name == "id" or name == "reference_type" or name == "key" or name == "updated_at" or name == "created_at" or value.nil? or value == ""
       if name.include? "_id"
         if name.include? "journal"
-          bibtex += "journal = {" + journal.name + "},\n"
+          bibtex += "journal = {" + special_chars(journal.name) + "},\n"
         elsif name.include? "publisher"
-          bibtex += "publisher = {" + publisher.name + "},\n"
+          bibtex += "publisher = {" + special_chars(publisher.name) + "},\n"
         elsif name.include? "series"
-          bibtex += "series = {" + series.name + "},\n"
+          bibtex += "series = {" + special_chars(series.name) + "},\n"
         elsif name.include? "organization"
-          bibtex += "organization = {" + organization.name + "},\n"
+          bibtex += "organization = {" + special_chars(organization.name) + "},\n"
         end
       else
-        bibtex += name + " = {" + value.to_s + "},\n"
+        bibtex += name + " = {" + special_chars(value.to_s) + "},\n"
       end
      end
    end
    bibtex += "}"
    return bibtex
+  end
+
+  def special_chars(string)
+    string = string.gsub(/[åäö]/, 'å' => '\aa', 'ä' => '\"{a}', 'ö' => '\"{o}')
   end
 end
