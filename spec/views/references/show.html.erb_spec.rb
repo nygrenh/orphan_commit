5b83dcabaf55a0de0dea663fe2ae05bacb05c39c
Sonopa <juzax@hotmail.com>
2014-04-08 16:43:12 +0300

    Removed failing autogenerated test

--- spec/views/references/show.html.erb_spec.rb
+++ spec/views/references/show.html.erb_spec.rb
@@ -5,8 +5,4 @@ describe "references/show" do
     @reference = assign(:reference, stub_model(Reference))
   end
 
-  it "renders attributes in <p>" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-  end
 end
