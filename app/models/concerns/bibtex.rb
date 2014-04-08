11fc2972d7567a6dc74f7a3546eb96a5aef20885
Sonopa <juzax@hotmail.com>
2014-04-08 15:58:48 +0300

    refactor

--- app/models/concerns/bibtex.rb
+++ app/models/concerns/bibtex.rb
@@ -1,14 +1,13 @@
 module Bibtex
   extend ActiveSupport::Concern
 
-  def generate_bibtex(reference)
-    bibtex = "@" + reference.reference_type + "{"
-    if reference.key == ""
+  def generate_bibtex()
+    bibtex = "@" + reference_type + "{"
+    if key == ""
       bibtex += "\n"
     else
-      bibtex += reference.key + ",\n"
+      bibtex += key + ",\n"
     end
-    authors = reference.authors
     bibtex += "author = {"
     authors.each_with_index do |author, index|
       if (index+1 == authors.length)
@@ -17,18 +16,17 @@ module Bibtex
         bibtex += author.name + "and"
       end
     end
-    attributes = reference.attributes
     attributes.each_pair do |name, value|
     unless name == "id" or name == "reference_type" or name == "key" or name == "updated_at" or name == "created_at" or value.nil? or value == ""
       if name.include? "_id"
         if name.include? "journal"
-          bibtex += "journal = {" + reference.journal.name + "},\n"
+          bibtex += "journal = {" + journal.name + "},\n"
         elsif name.include? "publisher"
-          bibtex += "publisher = {" + reference.publisher.name + "},\n"
+          bibtex += "publisher = {" + publisher.name + "},\n"
         elsif name.include? "series"
-          bibtex += "series = {" + reference.series.name + "},\n"
+          bibtex += "series = {" + series.name + "},\n"
         elsif name.include? "organization"
-          bibtex += "organization = {" + reference.organization.name + "},\n"
+          bibtex += "organization = {" + organization.name + "},\n"
         end
       else
         bibtex += name + " = {" + value.to_s + "},\n"
@@ -38,5 +36,4 @@ module Bibtex
    bibtex += "}"
    return bibtex
   end
-
 end
