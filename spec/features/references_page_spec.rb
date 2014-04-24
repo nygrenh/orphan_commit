ed16ca1fe86fb9024cb7077b05237dfd7688a569
Eveliina <pevetiainen.ep@hotmail.com>
2014-04-24 22:45:04 +0300

    Added tests for search

--- spec/features/references_page_spec.rb
+++ spec/features/references_page_spec.rb
@@ -52,22 +52,7 @@ describe "References page" do
   describe "with right parameters given" do
 
     it "user can create a new article reference" do
-      visit new_article_path
-
-      fill_in('reference_title', with: 'Best article')
-      fill_in('reference_year', with: '2014')
-      fill_in('reference_journal_name', with: 'Best Journal')
-      fill_in('reference_authors_names', with: 'P. Parsa')
-      fill_in('reference_volume', with: '54')
-      fill_in('reference_number', with: '7')
-      fill_in('reference_pages', with: '250--580')
-      fill_in('reference_month', with: '3')
-      fill_in('reference_note', with: 'New Note')
-      fill_in('reference_publisher_name', with: 'Publisher first')
-      fill_in('reference_address', with: 'Main Street 7')
-      fill_in('reference_key', with: 'fdtgvj863')
-
-      click_button('Create Reference')
+      create_article_reference(2014)
 
       expect(page).to have_content "Reference was successfully created."
       expect(Reference.count).to eq(1)
@@ -76,23 +61,7 @@ describe "References page" do
 
     it "user can create a new book reference" do
 
-      visit new_book_path
-
-      fill_in('reference_title', with: 'Best book')
-      fill_in('reference_year', with: '2014')
-      fill_in('reference_authors_names', with: 'P. Puska')
-      fill_in('reference_publisher_name', with: 'Best publisher')
-      fill_in('reference_editors_names', with: 'A. Duck')
-      fill_in('reference_edition', with: '1.')
-      fill_in('reference_volume', with: '250')
-      fill_in('reference_number', with: '3')
-      fill_in('reference_series', with: 'IMPORTANT')
-      fill_in('reference_month', with: '5')
-      fill_in('reference_note', with: 'Remember this')
-      fill_in('reference_address', with: 'Main Street 3')
-      fill_in('reference_key', with: 'XTY567')
-
-      click_button('Create Reference')
+      create_book_reference(2014)
 
       expect(page).to have_content "Reference was successfully created."
       expect(Reference.count).to eq(1)
@@ -100,25 +69,7 @@ describe "References page" do
 
     it "user can create a new inproceedings reference" do
 
-      visit new_inproceedings_path
-
-      fill_in('reference_title', with: 'Best inproceedings')
-      fill_in('reference_year', with: '2014')
-      fill_in('reference_authors_names', with: 'P. Puska & Co.')
-      fill_in('reference_booktitle', with: 'Best book')
-      fill_in('reference_editors_names', with: 'A. Duck')
-      fill_in('reference_series', with: '1.')
-      fill_in('reference_volume', with: '250')
-      fill_in('reference_number', with: '3')
-      fill_in('reference_pages', with: '125--587')
-      fill_in('reference_month', with: '5')
-      fill_in('reference_note', with: 'Remember this')
-      fill_in('reference_organization', with: 'Inproceed Org')
-      fill_in('reference_publisher_name', with: 'Publisher first')
-      fill_in('reference_address', with: 'Main Street 5')
-      fill_in('reference_key', with: '1245khhj')
-
-      click_button('Create Reference')
+      create_inproceedings_reference(2014)
 
       expect(page).to have_content "Reference was successfully created."
       expect(Reference.count).to eq(1)
@@ -129,22 +80,7 @@ describe "References page" do
   describe "with wrong parameters given" do
 
     it "user cannot create a new article reference" do
-      visit new_article_path
-
-      fill_in('reference_title', with: '')
-      fill_in('reference_year', with: '-2014')
-      fill_in('reference_journal_name', with: 'Best Journal')
-      fill_in('reference_authors_names', with: 'P. Parsa')
-      fill_in('reference_volume', with: '-54')
-      fill_in('reference_number', with: '7')
-      fill_in('reference_pages', with: '250--580')
-      fill_in('reference_month', with: '3')
-      fill_in('reference_note', with: 'New Note')
-      fill_in('reference_publisher_name', with: 'Publisher first')
-      fill_in('reference_address', with: 'Main Street 7')
-      fill_in('reference_key', with: 'fdtgvj863')
-
-      click_button('Create Reference')
+      create_article_reference(-2586)
       expect(page).to have_content "prohibited this reference from being saved"
       expect(Reference.count).to eq(0)
 
@@ -152,23 +88,7 @@ describe "References page" do
 
     it "user cannot create a new book reference" do
 
-      visit new_book_path
-
-      fill_in('reference_title', with: '')
-      fill_in('reference_year', with: '-2014')
-      fill_in('reference_authors_names', with: 'P. Puska')
-      fill_in('reference_publisher_name', with: 'Best publisher')
-      fill_in('reference_editors_names', with: 'A. Duck')
-      fill_in('reference_edition', with: '1.')
-      fill_in('reference_volume', with: '-250')
-      fill_in('reference_number', with: '-3')
-      fill_in('reference_series', with: 'IMPORTANT')
-      fill_in('reference_month', with: '5')
-      fill_in('reference_note', with: 'Remember this')
-      fill_in('reference_address', with: 'Main Street 3')
-      fill_in('reference_key', with: 'XTY567')
-
-      click_button('Create Reference')
+     create_book_reference(-2586)
 
       expect(page).to have_content "prohibited this reference from being saved"
       expect(Reference.count).to eq(0)
@@ -176,25 +96,7 @@ describe "References page" do
 
     it "user cannot create a new inproceedings reference" do
 
-      visit new_inproceedings_path
-
-      fill_in('reference_title', with: '')
-      fill_in('reference_year', with: '-2014')
-      fill_in('reference_authors_names', with: 'P. Puska & Co.')
-      fill_in('reference_booktitle', with: 'Best book')
-      fill_in('reference_editors_names', with: 'A. Duck')
-      fill_in('reference_series', with: '1.')
-      fill_in('reference_volume', with: '-250')
-      fill_in('reference_number', with: '3')
-      fill_in('reference_pages', with: '125--587')
-      fill_in('reference_month', with: '-5')
-      fill_in('reference_note', with: 'Remember this')
-      fill_in('reference_organization', with: 'Inproceed Org')
-      fill_in('reference_publisher_name', with: 'Publisher first')
-      fill_in('reference_address', with: 'Main Street 5')
-      fill_in('reference_key', with: '1245khhj')
-
-      click_button('Create Reference')
+      create_inproceedings_reference(-2586)
 
       expect(page).to have_content "prohibited this reference from being saved"
       expect(Reference.count).to eq(0)
@@ -207,9 +109,7 @@ describe "References page" do
       it "gets the right error messages" do 
         visit new_inproceedings_path
         click_button('Create Reference')
-        expect(page).to have_content "Title can't be empty"
-        expect(page).to have_content "Authors can't be empty"
-        expect(page).to have_content "Year can't be empty"
+        main_expectations
         expect(page).to have_content "Booktitle can't be empty"
       end
     end
@@ -217,9 +117,7 @@ describe "References page" do
       it "gets the right error messages" do 
         visit new_book_path
         click_button('Create Reference')
-        expect(page).to have_content "Title can't be empty"
-        expect(page).to have_content "Authors can't be empty"
-        expect(page).to have_content "Year can't be empty"
+        main_expectations
         expect(page).to have_content "Publisher can't be empty"
       end
     end
@@ -227,9 +125,7 @@ describe "References page" do
       it "gets the right error messages" do 
         visit new_article_path
         click_button('Create Reference')
-        expect(page).to have_content "Title can't be empty"
-        expect(page).to have_content "Authors can't be empty"
-        expect(page).to have_content "Year can't be empty"
+        main_expectations
         expect(page).to have_content "Journal can't be empty"
       end
     end
@@ -237,25 +133,7 @@ describe "References page" do
 
   it "user can delete a reference" do
 
-    visit new_inproceedings_path
-
-    fill_in('reference_title', with: 'Best inproceedings')
-    fill_in('reference_year', with: '2014')
-    fill_in('reference_authors_names', with: 'P. Puska & Co.')
-    fill_in('reference_booktitle', with: 'Best book')
-    fill_in('reference_editors_names', with: 'A. Duck')
-    fill_in('reference_series', with: '1.')
-    fill_in('reference_volume', with: '250')
-    fill_in('reference_number', with: '3')
-    fill_in('reference_pages', with: '125--587')
-    fill_in('reference_month', with: '5')
-    fill_in('reference_note', with: 'Remember this')
-    fill_in('reference_organization', with: 'Inproceed Org')
-    fill_in('reference_publisher_name', with: 'Publisher first')
-    fill_in('reference_address', with: 'Main Street 5')
-    fill_in('reference_key', with: '1245khhj')
-
-    click_button('Create Reference')
+    create_inproceedings_reference(2014)
 
 
     visit references_path
@@ -291,6 +169,10 @@ describe "References page" do
         year += 1
       end
 
+      create_inproceedings_reference(2014)
+      create_book_reference(2014)
+      create_article_reference(2014)
+
       visit references_path
 
 
@@ -304,9 +186,6 @@ describe "References page" do
       expect(page).to have_content "Ripan raapustukset"
       expect(page).to have_content "Penan piirrokset2"
       expect(page).to have_content "Pekan kuvakirja"
-      expect(page).to have_content "Book"
-      expect(page).to have_content "Inproceedings"
-      expect(page).to have_content "Article"
 
       expect(page).not_to have_content "2001"
       expect(page).not_to have_content "2002"
@@ -323,9 +202,6 @@ describe "References page" do
       expect(page).not_to have_content "Ripan raapustukset"
       expect(page).not_to have_content "Penan piirrokset2"
       expect(page).to have_content "Pekan kuvakirja"
-      expect(page).to have_content "Book"
-      expect(page).to have_content "Inproceedings"
-      expect(page).to have_content "Article"
 
       expect(page).to have_content "2001"
       expect(page).to have_content "2002"
@@ -340,13 +216,310 @@ describe "References page" do
       fill_in('searchtext', with: "Pekka")
       click_button('Search')
 
+
       expect(page).to have_content "Pekan kuvakirja"
+      expect(page).to have_content "Penan piirrokset"
+      expect(page).to have_content "Penan piirrokset2"
+      expect(page).to have_content "Ripan raapustukset"
+      expect(page).to have_content "Ripan raapustukset2"
+
 
       expect(page).to have_content "2001"
+      expect(page).to have_content "2002"
+      expect(page).to have_content "2003"
+      expect(page).to have_content "2004"
+      expect(page).to have_content "2005"
 
 
     end
 
+    it "user can search reference with editor" do
+      select('editor', from:'attribute')
+      fill_in('searchtext', with: "A. Duck")
+      click_button('Search')
+
+
+     expectations("P. Puska & Co.")
+
+
+    end
+
+    it "user can search reference with journal" do
+      select('journal', from:'attribute')
+      fill_in('searchtext', with: "Best Journal")
+      click_button('Search')
+
+
+      expectations("P. Parsa")
+
+
+    end
+
+    it "user can search reference with publisher" do
+      select('publisher', from:'attribute')
+      fill_in('searchtext', with: "Best publisher")
+      click_button('Search')
+
+
+
+      expectations("P. Puska")
+
+
+    end
+
+    it "user can search reference with series" do
+      select('series', from:'attribute')
+      fill_in('searchtext', with: "IMPORTANT")
+      click_button('Search')
+
+
+
+      expectations("P. Puska")
+
+
+    end
+
+    it "user can search reference with organization" do
+      select('organization', from:'attribute')
+      fill_in('searchtext', with: "Inproceed Org")
+      click_button('Search')
+
+
+
+      expectations("P. Puska & Co.")
+
+
+    end
+
+    it "user can search reference with tag" do
+      select('tag', from:'attribute')
+      fill_in('searchtext', with: "1TAG")
+      click_button('Search')
+
+
+
+      expectations("P. Puska & Co.")
+
+
+    end
+
+    it "user can search reference with reference type" do
+      select('reference type', from:'attribute')
+      fill_in('searchtext', with: "Book")
+      click_button('Search')
+
+
+      expect(page).to have_content "Pekan kuvakirja"
+      expect(page).to have_content "Best book"
+      expect(page).to have_content "Penan piirrokset2"
+      expect(page).to have_content "Ripan raapustukset2"
+
+    end
+
+    it "user can search reference with booktitle" do
+      select('booktitle', from:'attribute')
+      fill_in('searchtext', with: "Best book")
+      click_button('Search')
+
+
+
+      expectations("P. Puska & Co.")
+
+
+    end
+
+    it "user can search reference with edition" do
+      select('edition', from:'attribute')
+      fill_in('searchtext', with: "2.")
+      click_button('Search')
+
+
+
+      expectations("P. Puska")
+
+
+    end
+
+    it "user can search reference with adress" do
+      select('address', from:'attribute')
+      fill_in('searchtext', with: "Main Street 5")
+      click_button('Search')
+
+
+
+      expectations("P. Puska & Co.")
+
+
+    end
+
+    it "user can search reference with key" do
+      select('key', from:'attribute')
+      fill_in('searchtext', with: "1245khhj")
+      click_button('Search')
+
+
+
+      expectations("P. Puska & Co.")
+
+
+    end
+
+    it "user can search reference with note" do
+      select('note', from:'attribute')
+      fill_in('searchtext', with: "Remember this")
+      click_button('Search')
+
+
+
+      expectations("P. Puska & Co.")
+
+
+    end
+
+    it "user can search reference with month" do
+      select('month', from:'attribute')
+      fill_in('searchtext', with: "5")
+      click_button('Search')
+
+
+
+      expectations("P. Puska & Co.")
+
+
+    end
+
+    it "user can search reference with pages" do
+      select('pages', from:'attribute')
+      fill_in('searchtext', with: "125--5873")
+      click_button('Search')
+
+
+
+      expectations("P. Puska & Co.")
+
+
+    end
+
+    it "user can search reference with number" do
+      select('number', from:'attribute')
+      fill_in('searchtext', with: "3")
+      click_button('Search')
+
+
+
+      expectations("P. Puska & Co.")
+
+
+    end
+
+    it "user can search reference with volume" do
+      select('volume', from:'attribute')
+      fill_in('searchtext', with: "250")
+      click_button('Search')
+
+
+
+      expectations("P. Puska & Co.")
+
+
+    end
+
+
+
+
   end
 
+
+
 end
+
+def expectations(name)
+  expect(page).to have_content name
+  expect(page).not_to have_content "Pekan kuvakirja"
+  expect(page).not_to have_content "Penan piirrokset"
+  expect(page).not_to have_content "Penan piirrokset2"
+  expect(page).not_to have_content "Ripan raapustukset"
+  expect(page).not_to have_content "Ripan raapustukset2"
+
+  expect(page).to have_content "2014"
+  expect(page).not_to have_content "2001"
+  expect(page).not_to have_content "2002"
+  expect(page).not_to have_content "2003"
+  expect(page).not_to have_content "2004"
+  expect(page).not_to have_content "2005"
+end
+
+
+
+def create_inproceedings_reference(year)
+
+  visit new_inproceedings_path
+
+  fill_in('reference_title', with: 'Best inproceedings')
+  fill_in('reference_year', with: year)
+  fill_in('reference_authors_names', with: 'P. Puska & Co.')
+  fill_in('reference_booktitle', with: 'Best book')
+  fill_in('reference_editors_names', with: 'A. Duck')
+  fill_in('reference_series', with: '3.')
+  fill_in('reference_volume', with: '250')
+  fill_in('reference_number', with: '3')
+  fill_in('reference_pages', with: '125--5873')
+  fill_in('reference_month', with: '5')
+  fill_in('reference_note', with: 'Remember this')
+  fill_in('reference_organization', with: 'Inproceed Org')
+  fill_in('reference_publisher_name', with: 'Publisher first')
+  fill_in('reference_address', with: 'Main Street 5')
+  fill_in('reference_key', with: '1245khhj')
+  fill_in('reference_tags', with: '1TAG')
+
+  click_button('Create Reference')
+end
+
+def create_book_reference(year)
+
+  visit new_book_path
+
+  fill_in('reference_title', with: 'Best book')
+  fill_in('reference_year', with: year)
+  fill_in('reference_authors_names', with: 'P. Puska')
+  fill_in('reference_publisher_name', with: 'Best publisher')
+  fill_in('reference_editors_names', with: 'A. Duck')
+  fill_in('reference_edition', with: '2.')
+  fill_in('reference_volume', with: '250')
+  fill_in('reference_number', with: '3')
+  fill_in('reference_series', with: 'IMPORTANT')
+  fill_in('reference_month', with: '5')
+  fill_in('reference_note', with: 'Remember this')
+  fill_in('reference_address', with: 'Main Street 3')
+  fill_in('reference_key', with: 'XTY567')
+
+  click_button('Create Reference')
+end
+
+def create_article_reference(year)
+  visit new_article_path
+
+  fill_in('reference_title', with: 'Best article')
+  fill_in('reference_year', with: year)
+  fill_in('reference_journal_name', with: 'Best Journal')
+  fill_in('reference_authors_names', with: 'P. Parsa')
+  fill_in('reference_volume', with: '54')
+  fill_in('reference_number', with: '7')
+  fill_in('reference_pages', with: '250--580')
+  fill_in('reference_month', with: '3')
+  fill_in('reference_note', with: 'New Note')
+  fill_in('reference_publisher_name', with: 'Publisher first')
+  fill_in('reference_address', with: 'Main Street 7')
+  fill_in('reference_key', with: 'fdtgvj863')
+
+  click_button('Create Reference')
+
+
+end
+
+def main_expectations
+  expect(page).to have_content "Title can't be empty"
+  expect(page).to have_content "Authors can't be empty"
+  expect(page).to have_content "Year can't be empty"
+end
+
